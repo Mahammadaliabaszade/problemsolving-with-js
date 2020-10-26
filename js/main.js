825e80fc5207fec94d05f35e4f249ff48b12a193
Mahammadaliabaszade <68659317+Mahammadaliabaszade@users.noreply.github.com>
2020-10-27 00:46:53 +0400

    problemsolve

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// let name = "maqa";
+// var let ferqi
+// console.log(name);
+// for (var i = 0; i <= 6; i++) {
+//     console.log(i);
+
+// }
+// console.log(i)
+
+// let reqem = 20;
+// let soz = "salamlar bakiya ";
+// let sozs = `salamlar ${reqem}`;
+// console.log(sozs);
+
+
+
+let password;
+let sual = prompt("who is there");
+
+
+
+
+if (sual == "admin") {
+    let password = prompt("passwordunuz nedir");
+    if (password == null) {
+        console.log("cancelled");
+    } else if (password = "themaster") {
+        console.log("welcome")
+    } else {
+        console.log("wrongpass")
+    }
+} else if (sual == null) {
+    console.log("Cancelled");
+
+} else {
+    console.log("idontknow");
+}
+
+// if (password == )
